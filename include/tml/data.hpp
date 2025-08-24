f7d2942915406fc046b6cbe4524ea2bcc6cc914d
KokoT <prikolkillercat@gmail.com>
2025-08-24 14:16:35 +0200

    trying to design data model. design tml::Number

---
+++ include/tml/data.hpp
@@ -0,0 +1,52 @@
+#pragma once
+#include"config.hpp"
+#include <type_traits>
+
+namespace tml
+{
+
+    template<typename T>
+    struct IsTMLObj : std::disjunction<
+        std::is_same<T, Bool>,
+        std::is_same<T, Number>,
+        std::is_same<T, String>,
+        std::is_same<T, List>, 
+        std::is_same<T, Object>> {};    
+
+    /// @brief Abstract tml object
+    class TML_API TMLObject {
+    public:
+        /// @brief return true if this TMLObject is T
+        template<typename T, typename = std::enable_if_t<IsTMLObj<T>::value>>
+        bool Is();
+    };
+
+    class TML_API Number : TMLObject {    
+    public:
+        enum class ValueType { Integer, Float };
+
+        Number(const Number& other);
+        Number(int64_t value);
+        Number(double value);
+
+        template<typename T>
+        T GetValue() const;
+        
+        template<typename T>
+        void SetValue(T value) const;
+
+        bool IsInteger() const;
+        bool IsFloat() const;
+
+        Number& operator=(const Number& other);
+        template<typename T>
+        Number& operator=(const T& other);
+    private:
+        union {
+            int64_t i;
+            double d;
+        } _value;
+        ValueType _type;
+    };
+
+} // namespace tml
---
+++ include/tml/reader.hpp
@@ -0,0 +1,9 @@
+#pragma once
+#include"config.hpp"
+
+namespace tml
+{
+    
+    
+
+} // namespace tml
